781f4102bc44fb2e23e1d784da92a24d430ac369
道廷途说 <daotin@qq.com>
2021-03-29 17:42:30 +0800

    intro.js demo

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -13,16 +13,103 @@
     <div class="top">top</div>
     <div class="middle"></div>
     <div class="bottom">bottom</div>
+
+    <div class="box">div Box</div>
+
+    <div class="shuru">
+      <input type="text" />
+      <button @click="twoStep">确认</button>
+    </div>
+
+    123
+
+    <a class="one-step" href="javascript:;" @click.stop="oneStep" data-step="1"
+      >点击第一步的超链接</a
+    >
   </div>
 </template>
 
 <script>
+import introJs from "intro.js";
+import Driver from "driver.js";
+
 export default {
   name: "test",
   props: {
     msg: String,
   },
-  methods: {},
+  methods: {
+    oneStep() {
+      introJs()
+        .setOptions({
+          steps: [
+            {
+              title: "第二步标题",
+              element: document.querySelector(".shuru"),
+              intro: "第二步：请输入内容，点击确认",
+            },
+          ],
+          prevLabel: "上一步",
+          nextLabel: "下一步",
+          doneLabel: "完成",
+          showBullets: false,
+          exitOnOverlayClick: false,
+          disableInteraction: true,
+        })
+        .start();
+    },
+    twoStep() {
+      introJs()
+        .setOptions({
+          steps: [
+            {
+              title: "第3步标题",
+              element: document.querySelector(".shuru button"),
+              intro: "第3步：恭喜你，完成注册",
+            },
+          ],
+          hidePrev: true,
+          doneLabel: "完成",
+          showBullets: false,
+          exitOnOverlayClick: false,
+          disableInteraction: true,
+        })
+        .start();
+    },
+  },
+  mounted() {
+    introJs()
+      .setOptions({
+        steps: [
+          {
+            title: "Welcome",
+            intro: "欢迎来到Bangboss测评 👋",
+            prevLabel: "上一步11",
+          },
+          {
+            title: "第一步标题",
+            element: document.querySelector("a.one-step"),
+            intro: "1111111",
+          },
+        ],
+        prevLabel: "上一步",
+        nextLabel: "下一步",
+        doneLabel: "开始",
+        showBullets: false,
+        exitOnOverlayClick: false,
+        disableInteraction: true,
+      })
+      .start();
+
+    // const driver = new Driver();
+    // driver.highlight({
+    //   element: "",
+    //   popover: {
+    //     title: "Title for the Popover",
+    //     description: "Description for it",
+    //   },
+    // });
+  },
 };
 </script>
 
@@ -39,6 +126,7 @@ export default {
       height: 80px;
       background: pink;
       // margin-left: 100px;
+      z-index: 1000000000000000000000;
     }
   }
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
 
-import router from "./router";
+import router from './router';
+import 'intro.js/introjs.css';
+import 'driver.js/dist/driver.min.css';
 
-import "./resources/less/reset.less";
+import './resources/less/reset.less';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: (h) => h(App)
-}).$mount("#app");
+    router,
+    render: (h) => h(App)
+}).$mount('#app');
